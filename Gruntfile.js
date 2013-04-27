fe6e057c8398f18c93a61e9ff4d85e17bcba2d5f
David Rubert <david.rubert@gmail.com>
2013-04-27 10:38:18 +0200

    Added Gruntfile, updated package.json, moved examples to examples directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,25 @@
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-karma');
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
             dist: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    'dist/<%= pkg.name %>.min.js': ['src/angular-leaflet-directive.js']
                 }
             }
         },
         jshint: {
-            files: ['Gruntfile.js', '*.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
             options: {
                 // options here to override JSHint defaults
                 globals: {
@@ -33,19 +30,19 @@ module.exports = function(grunt) {
                 }
             }
         },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            }
+        },
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint', 'qunit']
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
     grunt.registerTask('test', ['jshint']);
-
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
