99ee1313e2ad880c22a5160cd8fc4920b1dbf040
Guille <guille@ggalmazor.com>
2013-05-22 16:18:27 +0200

    Extracted various default values into a defaults map Commented out function parameters that are not used Removed tileLayer and maxzoom values from scope creation as these values can be put directly as attributes in order to minimize code in the controller. Inlined $el variable Normalized storage of directive's model into $scope object Normalized exposure of Leaflet objects into a $scope.leaflet object Extracted functions for basic setup operations of centering the map, adding & binding markers and polylines Simplified checking for presence of model values (markers, path, etc.) Added L.icon into L.Marker definition to better handle different marker images and dimensions (pending to make this configurable, but you can place whatever images in your app's img folder) Fixed polyline handling:  - Now the color is also initialized at polyline creation  - Leaflet expects a coordinates or L.LatLng object array into L.polyline.setLatLng. Now lat & lng properties in each latlngs entry is still enforced, but it's casted to L.LatLng before passing the collection to the polyline. Adapted specs

--- src/angular-leaflet-directive.js
+++ src/angular-leaflet-directive.js
@@ -1,196 +1,198 @@
 var leafletDirective = angular.module("leaflet-directive", []);
 
 leafletDirective.directive("leaflet", ["$http", "$log", function ($http, $log) {
+    var defaults = {
+        maxZoom: 10,
+        tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        icon: {
+            url: 'img/leaflet/marker-icon.png',
+            retinaUrl: 'img/leaflet/marker-icon-2x.png',
+            size: [25, 41],
+            anchor: [12, 40],
+            popup: [-3, -76],
+            shadow: {
+                url: 'img/leaflet/marker-shadow.png',
+                retinaUrl: 'img/leaflet/marker-shadow.png',
+                size: [41, 41],
+                anchor: [12, 40]
+            }
+        },
+        path: {
+            weight: 10,
+            opacity: 1
+        }
+    };
     return {
         restrict: "E",
         replace: true,
         transclude: true,
         scope: {
-            center: "=center",
-            tilelayer: "=tilelayer",
-            markers: "=markers",
-            path: "=path",
-            maxZoom: "@maxzoom"
+            center: '=center',
+            markers: '=markers',
+            path: '=path'
         },
         template: '<div class="angular-leaflet-map"></div>',
-        link: function (scope, element, attrs, ctrl) {
-            var $el = element[0],
-                map = new L.Map($el);
-
-            // Expose the map object, for testing purposes
-            if (attrs.testing) {
-                scope.map = map;
-            }
-
-            // Set maxZoom from attrs
-            if (attrs.maxzoom){
-                scope.maxZoom = parseInt(attrs.maxzoom, 10);
-            }
-
-            // Set initial view
+        link: function ($scope, element, attrs /*, ctrl */) {
+            var map = new L.Map(element[0]);
             map.setView([0, 0], 1);
 
-            // Set tile layer
-            var tilelayer = scope.tilelayer || 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var maxZoom = scope.maxZoom || 12;
-            L.tileLayer(tilelayer, { maxZoom: maxZoom }).addTo(map);
+            $scope.leaflet = {};
+            $scope.leaflet.map = !!attrs.testing ? map : 'Add testing="testing" to <leaflet> tag to inspect this object';
+            $scope.leaflet.maxZoom = !!attrs.maxzoom ? parseInt(attrs.maxzoom, defaults.maxZoom) : defaults.maxZoom;
+            $scope.leaflet.tileLayer = attrs.tileLayer || defaults.tileLayer;
 
-            // Manage map center events
-            if (attrs.center && scope.center) {
+            L.tileLayer($scope.leaflet.tileLayer, { maxZoom: $scope.leaflet.maxZoom }).addTo(map);
 
-                if (scope.center.lat && scope.center.lng && scope.center.zoom) {
-                    map.setView([scope.center.lat, scope.center.lng], scope.center.zoom);
-                } else if (scope.center.autoDiscover === true) {
-                    map.locate({ setView: true, maxZoom: maxZoom });
-                }
+            setupCenter();
+            setupMarkers();
+            setupPath();
 
-                map.on("dragend", function(e) {
-                    scope.$apply(function (s) {
-                        s.center.lat = map.getCenter().lat;
-                        s.center.lng = map.getCenter().lng;
+            function setupCenter() {
+                if (!$scope.center)
+                    return;
+
+                if ($scope.center.lat && $scope.center.lng && $scope.center.zoom)
+                    map.setView([$scope.center.lat, $scope.center.lng], $scope.center.zoom);
+                else if ($scope.center.autoDiscover === true)
+                    map.locate({ setView: true, maxZoom: $scope.maxZoom });
+
+                map.on("dragend", function (/* event */) {
+                    $scope.$apply(function (scope) {
+                        scope.center.lat = map.getCenter().lat;
+                        scope.center.lng = map.getCenter().lng;
                     });
                 });
 
-                map.on("zoomend", function(e) {
-                    if (scope.center.zoom !== map.getZoom()){
-                        scope.$apply(function (s) {
+                map.on("zoomend", function (/* event */) {
+                    if ($scope.center.zoom !== map.getZoom()) {
+                        $scope.$apply(function (s) {
                             s.center.zoom = map.getZoom();
                         });
                     }
                 });
 
-                scope.$watch("center", function (center, oldValue) {
-                    if(center.lat && center.lng && center.zoom){
+                $scope.$watch("center", function (center /*, oldValue */) {
+                    if (center.lat && center.lng && center.zoom) {
                         map.setView([center.lat, center.lng], center.zoom);
                     }
                 }, true);
             }
 
-            if (attrs.markers && scope.markers) {
-                var createAndLinkMarker = function(key, scope) {
-                    var data = scope.markers[key];
-                    var marker = new L.marker(
-                        scope.markers[key],
-                        {
-                            draggable: data.draggable ? true:false
-                        }
-                    );
+            function setupMarkers() {
+                var markers = {};
+                $scope.leaflet.markers = !!attrs.testing ? markers : 'Add testing="testing" to <leaflet> tag to inspect this object';
+                if (!$scope.markers)
+                    return;
+                addMarkers($scope.markers, markers);
 
-                    if (data.message) {
-                        marker.bindPopup(data.message);
-                    }
-
-                    marker.on("dragend", function(e) {
-                        scope.$apply(function (s) {
-                            data.lat = marker.getLatLng().lat;
-                            data.lng = marker.getLatLng().lng;
-                        });
-                        if (data.message) {
-                            marker.openPopup();
-                        }
-                    });
-
-                    scope.$watch('markers.' + key, function(newval, oldval) {
-                        if (!newval) {
-                            map.removeLayer(markers[key]);
-                            delete leafletMarkers[key];
-                            if (attrs.testing) {
-                                delete scope.leafletMarkers[key];
-                            }
-                            return;
-                        }
-
-                        if (newval.draggable !== undefined && newval.draggable !== oldval.draggable) {
-                            if (newval.draggable === true) {
-                                marker.dragging.enable();
-                            } else {
-                                marker.dragging.disable();
-                            }
-                        }
-
-                        if (newval.focus !== undefined && newval.focus !== oldval.focus) {
-                            if (newval.focus === true) {
-                                marker.openPopup();
-                            } else {
-                                marker.closePopup();
-                            }
-                        }
-
-                        if (newval.message !== undefined && newval.message !== oldval.message) {
-                            marker.bindPopup(newval);
-                        }
-
-                        if (newval.lat !== oldval.lat || newval.lng !== oldval.lng) {
-                            marker.setLatLng(new L.LatLng(newval.lat, newval.lng));
-                        }
-                    }, true);
-
-                    return marker;
-                }; // end of create and link marker
-
-                var leafletMarkers = {};
-
-                // Expose the markers object, for testing purposes
-                if (attrs.testing) {
-                    scope.leafletMarkers = {};
-                }
-
-                // Create the initial objects
-                for (var key in scope.markers) {
-                    var marker = createAndLinkMarker(key, scope);
-                    map.addLayer(marker);
-                    leafletMarkers[key] = marker;
-                    if (attrs.testing) {
-                        scope.leafletMarkers[key] = marker;
-                    }
-                }
-
-                scope.$watch("markers", function(newMarkerList) {
-                    // add new markers
-                    for (var key in newMarkerList) {
-                        if (leafletMarkers[key] === undefined) {
-                            var marker = createAndLinkMarker(key, scope);
-                            map.addLayer(marker);
-                            leafletMarkers[key] = marker;
-                            if (attrs.testing) {
-                                scope.leafletMarkers[key] = marker;
-                            }
-                        }
-                    }
+                $scope.$watch("markers", function (newMarkers) {
+                    addMarkers(newMarkers, markers);
                 }, true);
-            } // if attrs.markers
+            }
 
-            if (attrs.path && scope.path) {
-                var polyline = new L.Polyline([], { weight: 10, opacity: 1});
+            function addMarkers(markers, currentMarkers) {
+                for (var name in markers)
+                    if (currentMarkers[name] === undefined) {
+                        var marker = buildMarker(name);
 
-                // For testing purposes
-                if (attrs.testing) {
-                    scope.polyline = polyline;
-                }
+                        marker.on("dragend", function (/* event */) {
+                            $scope.$apply(function (/* scope */) {
+                                data.lat = marker.getLatLng().lat;
+                                data.lng = marker.getLatLng().lng;
+                            });
+                            if (data.message)
+                                marker.openPopup();
+                        });
+
+                        $scope.$watch('markers.' + name, function (data, oldData) {
+                            if (!data) {
+                                map.removeLayer(currentMarkers[name]);
+                                delete currentMarkers[name];
+                                return;
+                            }
+
+                            if (data.draggable !== undefined && data.draggable !== oldData.draggable)
+                                if (data.draggable === true)
+                                    marker.dragging.enable();
+                                else
+                                    marker.dragging.disable();
+
+                            if (data.focus !== undefined && data.focus !== oldData.focus)
+                                if (data.focus === true)
+                                    marker.openPopup();
+                                else
+                                    marker.closePopup();
+
+                            if (data.message !== undefined && data.message !== oldData.message)
+                                marker.bindPopup(data);
+
+                            if (data.lat !== oldData.lat || data.lng !== oldData.lng)
+                                marker.setLatLng(new L.LatLng(data.lat, data.lng));
+                        }, true);
+                        map.addLayer(marker);
+                        currentMarkers[name] = marker;
+                    }
+            }
+
+            function buildMarker(name) {
+                var data, marker;
+                data = $scope.markers[name];
+                marker = new L.marker(
+                        $scope.markers[name],
+                        {
+                            icon: buildIcon(),
+                            draggable: data.draggable ? true : false
+                        }
+                );
+                if (data.message)
+                    marker.bindPopup(data.message);
+                return marker;
+            }
+
+            function buildIcon() {
+                return L.icon({
+                    iconUrl: defaults.icon.url,
+                    iconRetinaUrl: defaults.icon.retinaUrl,
+                    iconSize: defaults.icon.size,
+                    iconAnchor: defaults.icon.anchor,
+                    popupAnchor: defaults.icon.popup,
+                    shadowUrl: defaults.icon.shadow.url,
+                    shadowRetinaUrl: defaults.icon.shadow.retinaUrl,
+                    shadowSize: defaults.icon.shadow.size,
+                    shadowAnchor: defaults.icon.shadow.anchor
+                });
+            }
+
+            function setupPath() {
+                // TODO Create as many polylines as paths defined in model
+                // TODO Manage opacity changes with another $watch block
+                if (!$scope.path)
+                    return;
+                $log.warn("[AngularJS - Leaflet] Creating polylines and adding them to the map will break the directive's scope's inspection in AngularJS Batarang");
+                var polyline = new L.Polyline([], { weight: defaults.path.weight, opacity: defaults.path.opacity});
+                $scope.leaflet.path = !!attrs.testing ? polyline : 'Add testing="testing" to <leaflet> tag to inspect this object';
 
                 map.addLayer(polyline);
-                scope.$watch("path.latlngs", function(latlngs) {
-                    for (var idx=0, length=latlngs.length; idx < length; idx++) {
-                        if (latlngs[idx] === undefined || latlngs[idx].lat === undefined || latlngs[idx].lng === undefined) {
-                            $log.warn("Bad path point inn the $scope.path array ");
-                            latlngs.splice(idx, 1);
-                        }
-                    }
-                    polyline.setLatLngs(latlngs);
+
+                $scope.$watch("path.latlngs", function (latlngs) {
+                    var leafletLatLngs = latlngs
+                            .filter(function (latlng) {
+                                return !!latlng.lat && !!latlng.lng;
+                            })
+                            .map(function (latlng) {
+                                return new L.LatLng(latlng.lat, latlng.lng);
+                            });
+                    polyline.setLatLngs(leafletLatLngs);
                 }, true);
 
-                scope.$watch("path.weight", function(weight) {
-                    polyline.setStyle({
-                        weight: weight
-                    });
+                $scope.$watch("path.weight", function (weight) {
+                    polyline.setStyle({ weight: weight });
                 }, true);
 
-                scope.$watch("path.color", function(color) {
-                    polyline.setStyle({
-                        color: color
-                    });
+                $scope.$watch("path.color", function (color) {
+                    polyline.setStyle({ color: color });
                 }, true);
-            } // end of attrs.path
-        } // end of link function
+            }
+        }
     };
 }]);
