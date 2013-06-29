59dadc21afb4002ca9c3441252f53e582752d01a
mildtaste <mild.taste@gmail.com>
2013-06-29 12:16:29 +0200

    added support for height and width attributes

--- src/angular-leaflet-directive.js
+++ src/angular-leaflet-directive.js
@@ -40,9 +40,12 @@ leafletDirective.directive("leaflet", ["$http", "$log", "$parse", function ($htt
         },
         template: '<div class="angular-leaflet-map"></div>',
         link: function ($scope, element, attrs /*, ctrl */) {
+
+            if (attrs.width) {element.css('width', attrs.width)};
+            if (attrs.height) {element.css('height', attrs.height)};
+
             $scope.leaflet = {};
             $scope.leaflet.maxZoom = !!(attrs.defaults && $scope.defaults && $scope.defaults.maxZoom) ? parseInt($scope.defaults.maxZoom, 10) : defaults.maxZoom;
-
             var map = new L.Map(element[0], {
                 maxZoom: $scope.leaflet.maxZoom});
             map.setView([0, 0], 1);
