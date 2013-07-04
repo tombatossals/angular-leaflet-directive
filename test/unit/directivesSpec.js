7499ab992eb562ac3c8c3c1ec957ecd710b85990
Sergey Kuchmistov <sergey.kuchmistov@gmail.com>
2013-07-05 00:56:23 +0300

    add minZoom option

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -25,6 +25,16 @@ describe('Directive: leaflet', function() {
         });
     });
 
+    it('should set the min zoom if specified', function() {
+        inject(function($rootScope, $compile) {
+            angular.extend($rootScope, { defaults: { minZoom: 4 } });
+            var element = angular.element('<leaflet defaults="defaults" testing="testing"></leaflet>');
+            element = $compile(element)($rootScope);
+            var map = element.scope().leaflet.map;
+            expect(map.getMinZoom()).toEqual(4);
+        });
+    });
+
     it('should set tileLayer options if specified', function() {
         inject(function($rootScope, $compile) {
             angular.extend($rootScope, {
