d6fcda69c4fa8795f8f9b8a55a5a75ef4caf8936
Grant Lucas <contact@grantlucas.com>
2013-08-19 20:57:20 -0400

    Updated karma config to export a function
    http://karma-runner.github.io/0.10/config/configuration-file.html
    
    Was receiving errors when trying to run karma tests. Had to update the
    config file to export a function and use the jasmine framework.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,53 +1,57 @@
+module.exports = function(karma) {
+    karma.configure({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-// base path, that will be used to resolve files and exclude
-basePath = '../';
+        // list of files / patterns to load in the browser
+        files: [
+            'test/lib/leaflet.js',
+            'test/lib/angular/angular.js',
+            'test/lib/angular/angular-mocks.js',
+            'src/angular-leaflet-directive.js',
+            'test/unit/**/*.js'
+        ],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'test/lib/leaflet.js',
-  'test/lib/angular/angular.js',
-  'test/lib/angular/angular-mocks.js',
-  'src/angular-leaflet-directive.js',
-  'test/unit/**/*.js'
-];
+        // Frameworks
+        frameworks: ["jasmine"],
 
-// list of files to exclude
-exclude = [
-];
+        // list of files to exclude
+        exclude: [
+        ],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = [
-  'PhantomJS'
-];
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari
+        // - PhantomJS
+        browsers: [
+            'Firefox'
+        ],
 
-// test results reporter to use
-// possible values: dots || progress
-reporter = ['progress', 'growl'];
+        // test results reporter to use
+        // possible values: dots || progress
+        reporter: ['progress', 'growl'],
 
-// web server port
-port = 9018;
+        // web server port
+        port: 9018,
 
-// cli runner port
-runnerPort = 9100;
+        // cli runner port
+        runnerPort: 9100,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: LOG_INFO,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
