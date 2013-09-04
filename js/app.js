7014419c5a4869b3ff533a9a1cb6c7338bce7d77
David Rubert <david.rubert@gmail.com>
2013-09-04 20:15:14 +0200

    Advances on the basic-examples page

--- js/app.js
+++ js/app.js
@@ -16,13 +16,19 @@
 
     app.controller("MainController", [ '$scope', '$route', '$routeParams', '$location', function($scope, $route, $routeParams, $location) {
         $scope.$watch(function() { return $location.path(); }, function(value) {
-            $scope.activeTab = $location.path().replace(/^\//, "");
+            if (!value) return;
+            var section = value.match(/^\/([^\/])*/);
+            if (section.length > 0) {
+                $scope.activeTab = section[0].replace(/^\//, "");
+            }
         });
 
-        if ($routeParams.example) {
+        $scope.$watch(function() { return $routeParams.example ; }, function(value) {
             $scope.exampleTab = $routeParams.example;
-        }
+        });
+    }]);
 
+    app.controller("HeaderController", [ '$scope', function($scope) {
         angular.extend($scope, {
             center: {
                 lat: 40.095,
@@ -32,62 +38,91 @@
         });
     }]);
 
-})(window.angular);
-
-function DragMarkerController($scope) {
-    angular.extend($scope, {
-        center: {
-            lat: 40.095,
-            lng: -3.823,
-            zoom: 4
-        },
-        markers: {
-            Madrid: {
+    app.controller("CenterController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            center: {
                 lat: 40.095,
                 lng: -3.823,
-                message: "Drag me to your position",
-                focus: true,
-                draggable: true
+                zoom: 4
             }
-        }
-    });
-}
+        });
+    }]);
 
-function PathController($scope) {
-    angular.extend($scope, {
-        center: {
-            lat: 53,
-            lng: -3,
-            zoom: 6
-        },
-        paths: {
-            p1: {
-                color: '#008000',
-                weight: 3,
-                latlngs: [
-                    { lat: 51.50, lng: -0.082 },
-                    { lat: 51.751, lng: -1.255 },
-                    { lat: 53.48, lng: -2.24 },
-                ],
+    app.controller("CustomParametersController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            london: {
+                lat: 51.505,
+                lng: -0.09,
+                zoom: 8
+            },
+            defaults: {
+                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+                tileLayerOptions: {
+                    opacity: 0.9,
+                    detectRetina: true,
+                    reuseTiles: true,
+                },
+                scrollWheelZoom: false
             }
-        },
-    });
-}
+        });
+    }]);
 
-function LegendController($scope) {
-    angular.extend($scope, {
-        london: {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 12
-        },
-        legend: {
-            position: 'bottomleft',
-            colors: [ '#ff0000', '#28c9ff', '#0000ff', '#ecf386' ],
-            labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
-        },
-        defaults: {
-            tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
-        }
-    });
-};
+    app.controller("DraggingMarkerController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 40.095,
+                lng: -3.823,
+                zoom: 4
+            },
+            markers: {
+                Madrid: {
+                    lat: 40.095,
+                    lng: -3.823,
+                    message: "Drag me to another position",
+                    focus: true,
+                    draggable: true
+                }
+            }
+        });
+    }]);
+
+    app.controller("PathController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 53,
+                lng: -3,
+                zoom: 6
+            },
+            paths: {
+                p1: {
+                    color: '#008000',
+                    weight: 3,
+                    latlngs: [
+                        { lat: 51.50, lng: -0.082 },
+                        { lat: 51.751, lng: -1.255 },
+                        { lat: 53.48, lng: -2.24 },
+                    ],
+                }
+            },
+        });
+    }]);
+
+    app.controller("LegendController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            london: {
+                lat: 51.505,
+                lng: -0.09,
+                zoom: 12
+            },
+            legend: {
+                position: 'bottomleft',
+                colors: [ '#ff0000', '#28c9ff', '#0000ff', '#ecf386' ],
+                labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
+            },
+            defaults: {
+                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
+            }
+        });
+    }]);
+
+})(window.angular);
