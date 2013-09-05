917d06cf6021bfbc726f88635f9b5ca0485255f6
tombatossals <david.rubert@gmail.com>
2013-09-05 23:01:17 +0200

    More examples

--- js/app.js
+++ js/app.js
@@ -75,21 +75,129 @@
         });
     }]);
 
-    app.controller("DraggingMarkerController", [ '$scope', function($scope) {
+    app.controller("DraggingMarkersController", [ '$scope', function($scope) {
         angular.extend($scope, {
             center: {
                 lat: 40.095,
                 lng: -3.823,
-                zoom: 4
+                zoom: 6
             },
             markers: {
                 Madrid: {
                     lat: 40.095,
                     lng: -3.823,
-                    message: "Drag me to another position",
+                    message: "This is Madrid. Drag me to another position",
                     focus: true,
                     draggable: true
+                },
+                Barcelona: {
+                    lat: 41.38,
+                    lng: 2.18,
+                    message: "This is Barcelona. You can't drag me",
+                    focus: false,
+                    draggable: false
                 }
+            }
+        });
+    }]);
+
+    app.controller("MainMarkerController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            oslo: {
+                lat: 59.91,
+                lng: 10.75,
+                zoom: 12
+            },
+            marker: {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: true,
+                draggable: false
+            }
+        });
+    }]);
+
+    app.controller("EventsController", [ '$scope', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 52.374004,
+                lng: 4.890359,
+                zoom: 7
+            }
+        });
+
+        $scope.eventDetected = "No events yet...";
+
+        $scope.$on('leafletDirectiveMap.zoomstart', function(event){
+            $scope.eventDetected = "ZoomStart";
+        });
+
+        $scope.$on('leafletDirectiveMap.drag', function(event){
+            $scope.eventDetected = "Drag";
+        });
+
+        $scope.$on('leafletDirectiveMap.click', function(event){
+            $scope.eventDetected = "Click";
+        });
+
+        $scope.$on('leafletDirectiveMap.mousemove', function(event){
+            $scope.eventDetected = "MouseMove";
+        });
+    }]);
+
+    app.controller("CustomizeMarkersController", [ '$scope', function($scope) {
+        var local_icons = {
+            leaf_icon: L.icon({
+                iconUrl: 'img/leaf-green.png',
+                shadowUrl: 'img/leaf-shadow.png',
+                 iconSize:     [38, 95], // size of the icon
+                shadowSize:   [50, 64], // size of the shadow
+                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                shadowAnchor: [4, 62],  // the same for the shadow
+                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            }),
+            default_icon: L.icon({
+                iconUrl: 'http://cdn.leafletjs.com/leaflet-0.6.4/images/marker-icon.png',
+                shadowUrl: 'http://cdn.leafletjs.com/leaflet-0.6.4/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 40],
+                popupAnchor: [0, -40],
+                shadowSize: [41, 41],
+                shadowAnchor: [12, 40]
+            }),
+            div_icon: L.divIcon({
+                iconSize: [200, 0],
+                html: 'Using <strong>Bold text as an icon</strong>:',
+                popupAnchor:  [0, 0]
+            }),
+            object_icon: {
+                iconUrl: 'http://leafletjs.com/docs/images/leaf-orange.png',
+                shadowUrl: 'http://leafletjs.com/docs/images/leaf-shadow.png',
+                iconSize:     [38, 95],
+                shadowSize:   [50, 64],
+                iconAnchor:   [22, 94],
+                shadowAnchor: [4, 62]
+            },
+        }
+
+        angular.extend($scope, {
+            icons: local_icons
+        });
+
+        angular.extend($scope, {
+            lisboa: {
+                lat: 51.505,
+                lng: -0.09,
+                zoom: 8
+            },
+            markers: {
+                m1: {
+                    lat: 51.505,
+                    lng: -0.09,
+                    message: "I'm a static marker",
+                    icon: local_icons.default_icon,
+                },
             }
         });
     }]);
