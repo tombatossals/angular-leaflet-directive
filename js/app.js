1acada702c6704f50b6f86b5832f3d1b822882c7
tombatossals <david.rubert@gmail.com>
2013-09-09 21:25:50 +0200

    Already finished the redesign of the main page.

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@
 
     }]);
 
-    app.controller("HeaderController", [ '$scope', function($scope) {
+    app.controller("HeaderController", [ '$scope', '$location', function($scope, $location) {
         angular.extend($scope, {
             center: {
                 lat: 40.095,
@@ -43,8 +43,19 @@
                 zoom: 3
             },
             defaults: {
+                scrollWheelZoom: false,
+                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+                tileLayerOptions: {
+                    opacity: 0.9,
+                    detectRetina: true,
+                    reuseTiles: true,
+                },
                 scrollWheelZoom: false
             }
+        });
+
+        $scope.$on('leafletDirectiveMap.click', function(event){
+            $location.path("/");
         });
     }]);
 
@@ -88,7 +99,7 @@
                 Madrid: {
                     lat: 40.095,
                     lng: -3.823,
-                    message: "This is Madrid. Drag me to another position",
+                    message: "This is Madrid. But you can drag me to another position",
                     focus: true,
                     draggable: true
                 },
@@ -105,12 +116,12 @@
 
     app.controller("MainMarkerController", [ '$scope', function($scope) {
         angular.extend($scope, {
-            oslo: {
+            osloCenter: {
                 lat: 59.91,
                 lng: 10.75,
                 zoom: 12
             },
-            marker: {
+            osloMarker: {
                 lat: 59.91,
                 lng: 10.75,
                 message: "I want to travel here!",
@@ -151,8 +162,8 @@
     app.controller("CustomizeMarkersController", [ '$scope', function($scope) {
         var local_icons = {
             leaf_icon: L.icon({
-                iconUrl: 'img/leaf-green.png',
-                shadowUrl: 'img/leaf-shadow.png',
+                iconUrl: 'examples/img/leaf-green.png',
+                shadowUrl: 'examples/img/leaf-shadow.png',
                  iconSize:     [38, 95], // size of the icon
                 shadowSize:   [50, 64], // size of the shadow
                 iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
@@ -169,8 +180,8 @@
                 shadowAnchor: [12, 40]
             }),
             div_icon: L.divIcon({
-                iconSize: [200, 0],
-                html: 'Using <strong>Bold text as an icon</strong>:',
+                iconSize: [230, 0],
+                html: 'Using <strong>Bold text as an icon</strong>: Lisbon',
                 popupAnchor:  [0, 0]
             }),
             object_icon: {
@@ -188,18 +199,21 @@
         });
 
         angular.extend($scope, {
-            lisboa: {
-                lat: 51.505,
-                lng: -0.09,
+            lisbon: {
+                lat: 38.716,
+                lng: -9.13,
                 zoom: 8
             },
             markers: {
                 m1: {
-                    lat: 51.505,
-                    lng: -0.09,
+                    lat: 38.716,
+                    lng: -9.13,
                     message: "I'm a static marker",
                     icon: local_icons.default_icon,
                 },
+            },
+            defaults: {
+                scrollWheelZoom: false
             }
         });
     }]);
@@ -227,9 +241,9 @@
 
     app.controller("LegendController", [ '$scope', function($scope) {
         angular.extend($scope, {
-            london: {
-                lat: 51.505,
-                lng: -0.09,
+            amsterdam: {
+                lat: 52.35,
+                lng: 4.91,
                 zoom: 12
             },
             legend: {
@@ -238,9 +252,117 @@
                 labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
             },
             defaults: {
-                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
+                tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+                scrollWheelZoom: false
             }
         });
     }]);
 
+    app.controller("WorldMapController", [ '$scope', '$http', function($scope, $http) {
+
+        $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, leafletEvent) {
+            countryMouseover(leafletEvent);
+        });
+
+        $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
+            countryClick(featureSelected, leafletEvent);
+        });
+
+        var continentProperties= {
+                "009": {
+                        name: 'Oceania',
+                        colors: [ '#CC0066', '#993366', '#990066', '#CC3399', '#CC6699' ]
+                },
+                "019": {
+                        name: 'America',
+                        colors: [ '#006699', '#336666', '#003366', '#3399CC', '#6699CC' ]
+                },
+                "150": {
+                        name: 'Europe',
+                        colors: [ '#FF0000', '#CC3333', '#990000', '#FF3333', '#FF6666' ]
+                },
+                "002": {
+                        name: 'Africa',
+                        colors: [ '#00CC00', '#339933', '#009900', '#33FF33', '#66FF66' ]
+                },
+                "142": {
+                        name: 'Asia',
+                        colors: [ '#FFCC00', '#CC9933', '#999900', '#FFCC33', '#FFCC66' ]
+                },
+        };
+
+        angular.extend($scope, {
+            center: {
+                lat: 40.8471,
+                lng: 14.0625,
+                zoom: 2
+            },
+            defaults: {
+                scrollWheelZoom: false
+            },
+            legend: {
+                colors: [ '#CC0066', '#006699', '#FF0000', '#00CC00', '#FFCC00' ],
+                labels: [ 'Oceania', 'America', 'Europe', 'Africa', 'Asia' ]
+            }
+        });
+
+        function countryClick(country, event) {
+            console.log(country.properties.name);
+        }
+
+        // Get a country paint color from the continents array of colors
+        function getColor(country) {
+            if (!country || !country["region-code"]) {
+                return "#FFF";
+            }
+
+            var colors = continentProperties[country["region-code"]].colors;
+            var index = country["alpha-3"].charCodeAt(0) % colors.length ;
+            return colors[index];
+        }
+
+        function style(feature) {
+            return {
+                fillColor: getColor($scope.countries[feature.id]),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+            };
+        }
+
+        // Mouse over function, called from the Leaflet Map Events
+        function countryMouseover(leafletEvent) {
+            var layer = leafletEvent.target;
+            layer.setStyle({
+                weight: 2,
+                color: '#666',
+                fillColor: 'white'
+            });
+            layer.bringToFront();
+        }
+
+        // Get the countries data from a JSON
+        $http.get("examples/json/all.json").success(function(data, status) {
+
+            // Put the countries on an associative array
+            $scope.countries = {};
+            for (var i=0; i< data.length; i++) {
+                var country = data[i];
+                $scope.countries[country['alpha-3']] = country;
+            }
+
+            // Get the countries geojson data from a JSON
+            $http.get("examples/json/countries.geo.json").success(function(data, status) {
+                angular.extend($scope, {
+                    geojson: {
+                        data: data,
+                        style: style,
+                        resetStyleOnMouseout: true
+                    }
+                });
+            });
+        });
+    }]);
 })(window.angular);
