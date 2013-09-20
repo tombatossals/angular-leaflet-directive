8a01421ce3fb3d30b333000faf9ceb56cc4343b1
tombatossals <david.rubert@gmail.com>
2013-09-20 16:28:14 +0200

    Added the Google Maps example

--- js/app.js
+++ js/app.js
@@ -344,6 +344,35 @@
         });
     }]);
 
+    app.controller("GoogleMapsController", [ "$scope", function($scope) {
+        angular.extend($scope, {
+            berlin: {
+                lat: 52.52,
+                lng: 13.40,
+                zoom: 14
+            },
+            layers: {
+                baselayers: {
+                    googleTerrain: {
+                        name: 'Google Terrain',
+                        layerType: 'TERRAIN',
+                        type: 'google'
+                    },
+                    googleHybrid: {
+                        name: 'Google Hybrid',
+                        layerType: 'HYBRID',
+                        type: 'google'
+                    },
+                    googleRoadmap: {
+                        name: 'Google Streets',
+                        layerType: 'ROADMAP',
+                        type: 'google'
+                    }
+                }
+            }
+        });
+    }]);
+
     app.controller("WorldMapController", [ '$scope', '$http', function($scope, $http) {
 
         $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, leafletEvent) {
