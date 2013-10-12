d182e8b89655b6f33a318b48697d5f5cd3405437
tombatossals <david.rubert@gmail.com>
2013-10-12 12:06:00 +0200

    Updated examples with HTML5 DOCTYPE, updated main page examples, and updated library to latest version.

--- js/app.js
+++ js/app.js
@@ -375,64 +375,45 @@
     }]);
 
     app.controller("MultiLayerMapController", [ '$scope', '$http', function($scope, $http) {
-        var baselayers = {
+        var tiles = {
             osm: {
-                name: 'OpenStreetMap',
-                type: 'xyz',
-                url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                layerOptions: {
-                    subdomains: ['a', 'b', 'c'],
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    continuousWorld: true
-                }
+                url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             },
             cycle: {
-                name: 'OpenCycleMap',
-                type: 'xyz',
-                url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
-                layerOptions: {
-                    subdomains: ['a', 'b', 'c'],
-                    attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OpenCycleMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    continuousWorld: true
-                }
+                url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png'
             },
             night: {
-                name: 'Cloudmade Night Commander',
-                type: 'xyz',
                 url: 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
-                layerParams: {
+                options: {
                     key: '007b9471b4c74da4a6ec7ff43552b16f',
                     styleId: 999
-                },
-                layerOptions: {
-                    subdomains: ['a', 'b', 'c'],
-                    continuousWorld: true
                 }
             },
             tourist: {
-                name: 'Cloudmade Tourist',
-                type: 'xyz',
                 url: 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
-                layerParams: {
+                options: {
                     key: '007b9471b4c74da4a6ec7ff43552b16f',
                     styleId: 7
-                },
-                layerOptions: {
-                    subdomains: ['a', 'b', 'c'],
-                    continuousWorld: true
                 }
             }
         };
 
         $scope.setBaseLayer = function(key) {
-            for (var l in $scope.layers.baselayers) {
-                if ($scope.layers.baselayers.hasOwnProperty(l) && key !== l) {
-                    delete $scope.layers.baselayers[l];
-                    $scope.layers.baselayers[key] = baselayers[key];
+            var tile = tiles[key];
+            var url = tile.url;
+
+            if (tile.hasOwnProperty("options")) {
+                for (var key in tile.options) {
+                    if (tile.options.hasOwnProperty(key)) {
+                        url = url.replace("{" + key + "}", tile.options[key]);
+                    }
                 }
             }
+            $scope.tiles.url = url;
         };
 
+        var osm;
+        osm = angular.copy(tiles.osm, osm);
         angular.extend($scope, {
             center: {
                 lat: 40.8471,
@@ -442,9 +423,7 @@
             defaults: {
                 scrollWheelZoom: false
             },
-            layers: {
-                baselayers: { osm: baselayers.osm }
-            }
+            tiles: osm
         });
     } ]);
 
