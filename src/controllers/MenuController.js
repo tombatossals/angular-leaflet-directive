32bec89f80c750fa37302e6c8d97ed1a4dc1f18c
tombatossals <david.rubert@gmail.com>
2013-10-17 15:45:23 +0200

    Better way to manage the change of examples by mouse clicking on the menu

--- src/controllers/MenuController.js
+++ src/controllers/MenuController.js
@@ -1,58 +1,52 @@
-app.controller("MenuController", [ '$scope', function($scope) {
+app.controller("MenuController", [ '$scope', '$location', function($scope, $location) {
+
+    $scope.goExample = function (example) {
+        $location.path('/examples/' + example);
+    };
+
     $scope.menuItems = [
         {
             key: 'simple-map',
-            url: '#!/examples/simple-map',
             description: 'Simple Map'
         },
         {
             key: 'center',
-            url: '#!/examples/center',
             description: 'Center'
         },
         {
             key: 'custom-parameters',
-            url: '#!/examples/custom-parameters',
             description: 'Custom Parameters'
         },
         {
             key: 'events',
-            url: '#!/examples/events',
             description: 'Events'
         },
         {
             key: 'main-marker',
-            url: '#!/examples/main-marker',
             description: 'Main Marker'
         },
         {
             key: 'dragging-markers',
-            url: '#!/examples/dragging-markers',
             description: 'Dragging Markers'
         },
         {
             key: 'path',
-            url: '#!/examples/path',
             description: 'Path'
         },
         {
             key: 'geojson',
-            url: '#!/examples/geojson',
             description: 'GeoJSON'
         },
         {
             key: 'legend',
-            url: '#!/examples/legend',
             description: 'Legend'
         },
         {
             key: 'customized-markers',
-            url: '#!/examples/customized-markers',
             description: 'Customized markers'
         },
         {
             key: 'google-maps',
-            url: '#!/examples/google-maps',
             description: 'Google Maps'
         },
     ];
