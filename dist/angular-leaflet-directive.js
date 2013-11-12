7e52a129503242ad263ada23711c9ad9125c300e
Brett Willemsen <bzmwillemsen@gmail.com>
2013-11-11 22:27:35 -0500

    Added the ability for users to define their own geojson functions

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -496,7 +496,7 @@ angular.module("leaflet-directive").directive('legend', function ($log) {
     };
 });
 
-angular.module("leaflet-directive").directive('geojson', function ($log, $rootScope) {
+angular.module("leaflet-directive").directive('geojson', function($log, $rootScope) {
     return {
         restrict: "A",
         scope: false,
@@ -519,14 +519,11 @@ angular.module("leaflet-directive").directive('geojson', function ($log, $rootSc
                     }
 
                     if (isDefined(geojson.data)) {
-                        var resetStyleOnMouseout = geojson.resetStyleOnMouseout;
-
-                        leafletGeoJSON = L.geoJson(geojson.data, {
-                            style: geojson.style,
-                            onEachFeature: function(feature, layer) {
+                        var resetStyleOnMouseout = geojson.resetStyleOnMouseout,
+                            onEachFeatureDefault = function(feature, layer) {
                                 layer.on({
                                     mouseover: function(e) {
-                                        safeApply($scope, function() {
+                                        safeApply($scope, function () {
                                             geojson.selected = feature;
                                             $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseover', e);
                                         });
@@ -535,19 +532,31 @@ angular.module("leaflet-directive").directive('geojson', function ($log, $rootSc
                                         if (resetStyleOnMouseout) {
                                             leafletGeoJSON.resetStyle(e.target);
                                         }
-                                        safeApply($scope, function() {
+                                        safeApply($scope, function () {
                                             geojson.selected = undefined;
                                             $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseout', e);
                                         });
                                     },
                                     click: function(e) {
-                                        safeApply($scope, function() {
+                                        safeApply($scope, function () {
                                             $rootScope.$broadcast('leafletDirectiveMap.geojsonClick', geojson.selected, e);
                                         });
                                     }
                                 });
-                            }
-                        }).addTo(map);
+                            };
+
+                        if (!isDefined(geojson.options)) {
+                            // If geojson.options is not defined then set it to the standard.
+                            geojson.options = {
+                                style: geojson.style,
+                                onEachFeature: onEachFeatureDefault
+                            };
+                        } else if (!isDefined(geojson.options.onEachFeature)) {
+                            // If geojson.options is defined, but onEachFeature is not defined then set onEachFeature to the default
+                            geojson.options.onEachFeature = onEachFeatureDefault;
+                        }
+
+                        leafletGeoJSON = L.geoJson(geojson.data, geojson.options).addTo(map);
                     }
                 });
             });
