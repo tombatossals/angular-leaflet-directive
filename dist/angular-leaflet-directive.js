080fa82b7fc339b18fd9ec6f70eaa0cc22f02960
tombatossals <david.rubert@gmail.com>
2013-11-16 11:25:55 +0100

    New version of the library

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -4,7 +4,7 @@
 
 angular.module("leaflet-directive", []).directive('leaflet', function ($log, $q, leafletData, leafletMapDefaults, leafletHelpers, leafletEvents) {
     return {
-        restrict: "E",
+        restrict: "EA",
         replace: true,
         transclude: true,
         scope: {
