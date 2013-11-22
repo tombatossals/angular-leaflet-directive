524bc3c4037b70e2775b9dbe4e3e11ceae9a2a67
David Rubert <david.rubert@gmail.com>
2013-11-22 12:11:38 +0100

    Updated the getDefer function to solve the bug reported here by @n1313: https://github.com/tombatossals/angular-leaflet-directive/issues/194

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -293,61 +293,59 @@ angular.module("leaflet-directive").directive('geojson', function ($log, $rootSc
 
             controller.getMap().then(function(map) {
                 leafletScope.$watch("geojson", function(geojson) {
-                    if (isDefined(leafletGeoJSON)) {
+                    if (isDefined(leafletGeoJSON) && map.hasLayer(leafletGeoJSON)) {
                         map.removeLayer(leafletGeoJSON);
                     }
 
-                    if (!isDefined(geojson) || !isDefined(geojson.data)) {
+                    if (!(isDefined(geojson) && isDefined(geojson.data))) {
                         leafletData.setGeoJSON();
                         return;
                     }
 
-                    if (isDefined(geojson.data)) {
-                        var resetStyleOnMouseout = geojson.resetStyleOnMouseout,
-                            onEachFeatureDefault = function(feature, layer) {
-                                layer.on({
-                                    mouseover: function(e) {
-                                        safeApply(leafletScope, function() {
-                                            geojson.selected = feature;
-                                            $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseover', e);
-                                        });
-                                    },
-                                    mouseout: function(e) {
-                                        if (resetStyleOnMouseout) {
-                                            leafletGeoJSON.resetStyle(e.target);
-                                        }
-                                        safeApply(leafletScope, function() {
-                                            geojson.selected = undefined;
-                                            $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseout', e);
-                                        });
-                                    },
-                                    click: function(e) {
-                                        safeApply(leafletScope, function() {
-                                            $rootScope.$broadcast('leafletDirectiveMap.geojsonClick', geojson.selected, e);
-                                        });
+                    var resetStyleOnMouseout = geojson.resetStyleOnMouseout,
+                        onEachFeatureDefault = function(feature, layer) {
+                            layer.on({
+                                mouseover: function(e) {
+                                    safeApply(leafletScope, function() {
+                                        geojson.selected = feature;
+                                        $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseover', e);
+                                    });
+                                },
+                                mouseout: function(e) {
+                                    if (resetStyleOnMouseout) {
+                                        leafletGeoJSON.resetStyle(e.target);
                                     }
-                                });
-                            };
-                        if (!isDefined(geojson.options)) {
-                            // If geojson.options is not defined then set it to the standard.
-                            geojson.options = {
-                                style: geojson.style,
-                                onEachFeature: onEachFeatureDefault
-                            };
-                        } else if (!isDefined(geojson.options.onEachFeature)) {
-                            // If geojson.options is defined, but onEachFeature is not defined then set onEachFeature to the default
-                            geojson.options.onEachFeature = onEachFeatureDefault;
-                        }
-
-                        leafletGeoJSON = L.geoJson(geojson.data, geojson.options);
-
-                        if (leafletHelpers.LabelPlugin.isLoaded() && isDefined(geojson.options) && isDefined(geojson.options.label)) {
-                            leafletGeoJSON.bindLabel(geojson.options.label);
-                        }
-
-                        leafletData.setGeoJSON(leafletGeoJSON);
-                        leafletGeoJSON.addTo(map);
+                                    safeApply(leafletScope, function() {
+                                        geojson.selected = undefined;
+                                        $rootScope.$broadcast('leafletDirectiveMap.geojsonMouseout', e);
+                                    });
+                                },
+                                click: function(e) {
+                                    safeApply(leafletScope, function() {
+                                        $rootScope.$broadcast('leafletDirectiveMap.geojsonClick', geojson.selected, e);
+                                    });
+                                }
+                            });
+                        };
+                    if (!isDefined(geojson.options)) {
+                        // If geojson.options is not defined then set it to the standard.
+                        geojson.options = {
+                            style: geojson.style,
+                            onEachFeature: onEachFeatureDefault
+                        };
+                    } else if (!isDefined(geojson.options.onEachFeature)) {
+                        // If geojson.options is defined, but onEachFeature is not defined then set onEachFeature to the default
+                        geojson.options.onEachFeature = onEachFeatureDefault;
                     }
+
+                    leafletGeoJSON = L.geoJson(geojson.data, geojson.options);
+
+                    if (leafletHelpers.LabelPlugin.isLoaded() && isDefined(geojson.options) && isDefined(geojson.options.label)) {
+                        leafletGeoJSON.bindLabel(geojson.options.label);
+                    }
+
+                    leafletData.setGeoJSON(leafletGeoJSON);
+                    leafletGeoJSON.addTo(map);
                 });
             });
         }
@@ -1984,7 +1982,16 @@ angular.module("leaflet-directive").factory('leafletHelpers', function ($q) {
 
         getDefer: function(d, scopeId) {
             if (!angular.isDefined(scopeId)) {
-                scopeId = "main";
+                if (d.length > 1) {
+                    scopeId = "main";
+                } else {
+                    // Get the object key
+                    for (var i in d) {
+                        if (d.hasOwnProperty(i)) {
+                            scopeId = i;
+                        }
+                    }
+                }
             }
             var defer;
             if (!angular.isDefined(d[scopeId])) {
