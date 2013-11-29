1b2d21ee60b270a6b4fc8fcd0d4d42012fbedfd5
tombatossals <david.rubert@gmail.com>
2013-11-29 16:29:46 +0100

    Used grunt-bower-task as default

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,6 @@ module.exports = function(grunt) {
                     async: true
                 }
             },
-            bower_install: {
-                command: './node_modules/.bin/bower install'
-            },
             protractor_install: {
                 command: 'node ./node_modules/protractor/bin/install_selenium_standalone'
             },
@@ -205,6 +202,15 @@ module.exports = function(grunt) {
             }
         },
 
+        bower: {
+            install: {
+              //  options: {
+              //      targetDir: './bower_components',
+              //      cleanup: true
+              //  }
+            }
+        },
+
         concat: {
             dist: {
                 options: {
@@ -212,7 +218,6 @@ module.exports = function(grunt) {
                     footer: '\n}());'
                 },
                 src: [
-                    'bower_components/angularjs-scope.safeapply/src/Scope.SafeApply.js',
                     'src/directives/leaflet.js',
                     'src/directives/center.js',
                     'src/directives/tiles.js',
@@ -239,7 +244,7 @@ module.exports = function(grunt) {
                     'dist/angular-leaflet-directive.min.no-header.js'
                 ],
                 dest: 'dist/angular-leaflet-directive.min.js',
-            }
+            },
         }
     });
 
@@ -258,8 +263,7 @@ module.exports = function(grunt) {
     grunt.registerTask('coverage', ['karma:unit_coverage', 'open:coverage', 'connect:coverage']);
 
     //installation-related
-    grunt.registerTask('install', ['update', 'shell:bower_install', 'shell:protractor_install']);
-    grunt.registerTask('update', ['shell:npm_install']);
+    grunt.registerTask('install', ['shell:npm_install', 'bower:install', 'shell:protractor_install']);
 
     //defaults
     grunt.registerTask('default', ['watch:source']);
@@ -271,5 +275,5 @@ module.exports = function(grunt) {
     grunt.registerTask('serve', ['connect:webserver']);
 
     //travis
-    grunt.registerTask('travis', 'test:unit');
+    grunt.registerTask('travis', 'bower:install', 'test:unit');
 };
