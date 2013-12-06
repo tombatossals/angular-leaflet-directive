1b9914f5878bfd5ee3f52e75f5a233c7101815be
Fernando Witzke <pigroxalot@gmail.com>
2013-12-06 17:55:21 -0200

    #202 fixing issue when initializing with empty bounds and two way binding

--- test/unit/centerDirectiveSpec.js
+++ test/unit/centerDirectiveSpec.js
@@ -5,7 +5,7 @@
 /* jasmine specs for directives go here */
 
 describe('Directive: leaflet center', function() {
-    var $compile = null, $rootScope = null, $timeout, leafletData = null;
+    var $compile, $rootScope, $timeout, leafletData, center, scope;
 
     beforeEach(module('leaflet-directive'));
     beforeEach(inject(function(_$compile_, _$rootScope_, _$timeout_, _leafletData_){
@@ -19,11 +19,23 @@ describe('Directive: leaflet center', function() {
         $rootScope.$apply();
     }));
 
+    beforeEach(function(){
+        center = {
+            lat: 0.966,
+            lng: 2.02,
+            zoom: 4
+        };
+
+        scope = $rootScope.$new();
+        scope.center = center;
+    });
+
     it('should have default {[0, 0], 1} parameters on the map if not correctly defined', function() {
-        angular.extend($rootScope, {});
+        scope.center = {};
         var element = angular.element('<leaflet center="center"></leaflet>');
-        element = $compile(element)($rootScope);
-        $rootScope.$digest();
+        element = $compile(element)(scope);
+        scope.$digest();
+
         leafletData.getMap().then(function(map) {
             expect(map.getZoom()).toEqual(1);
             expect(map.getCenter().lat).toEqual(0);
@@ -32,15 +44,10 @@ describe('Directive: leaflet center', function() {
     });
 
     it('should update the map center if the initial center scope properties are set', function() {
-        var center = {
-            lat: 0.966,
-            lng: 2.02,
-            zoom: 4
-        };
-        angular.extend($rootScope, { center: center });
         var element = angular.element('<leaflet center="center"></leaflet>');
-        element = $compile(element)($rootScope);
-        $rootScope.$digest();
+        element = $compile(element)(scope);
+        scope.$digest();
+
         leafletData.getMap().then(function(map) {
             expect(map.getZoom()).toEqual(center.zoom);
             expect(map.getCenter().lat).toBeCloseTo(0.966);
@@ -49,29 +56,26 @@ describe('Directive: leaflet center', function() {
     });
 
     it('should update the map center if the scope center properties changes', function() {
-        var center = {
-            lat: 0.966,
-            lng: 2.02,
-            zoom: 4
-        };
-        angular.extend($rootScope, { center: center });
         var element = angular.element('<leaflet center="center"></leaflet>');
-        element = $compile(element)($rootScope);
+        element = $compile(element)(scope);
         var map;
         leafletData.getMap().then(function(leafletMap) {
             map = leafletMap;
         });
+        scope.$apply();
 
-        $rootScope.$apply();
         expect(map.getCenter().lat).toBeCloseTo(0.966);
         expect(map.getCenter().lng).toBeCloseTo(2.02);
         expect(map.getZoom()).toEqual(4);
+
         center.lat = 2.02;
         center.lng = 4.04;
         center.zoom = 8;
         $rootScope.$digest();
+
         expect(map.getCenter().lat).toBeCloseTo(2.02);
         expect(map.getCenter().lng).toBeCloseTo(4.04);
         expect(map.getZoom()).toEqual(8);
     });
+
 });
