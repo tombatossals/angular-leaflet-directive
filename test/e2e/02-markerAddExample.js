8ab8e449920bc493bb31f9aa6aa1711bbc8391b9
tombatossals <david.rubert@gmail.com>
2013-12-06 12:06:53 +0100

    Added a e2e tests to detect the errors on the marker-add-example, as reported by @fawick here:
    https://github.com/tombatossals/angular-leaflet-directive/issues/212

---
+++ test/e2e/02-markerAddExample.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Loading marker-add-example.html', function() {
+
+    var ptor, driver;
+    beforeEach(function() {
+        ptor = protractor.getInstance();
+        browser.get('/marker-add-example.html');
+        driver = ptor.driver;
+    });
+
+    it('should update the zoom value in the input if clicked the zoom control', function() {
+        ptor.wait(function() {
+            return ptor.isElementPresent(by.css('img.leaflet-tile-loaded'));
+        });
+
+        element(by.css('img.leaflet-tile-loaded')).click().then(function() {
+            expect(ptor.isElementPresent(by.css("img.leaflet-marker-icon"))).toBe(true);
+        });
+    });
+});
