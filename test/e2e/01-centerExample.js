5ce797e83ae99e6c6f227b7ec69b504b95076ce5
tombatossals <david.rubert@gmail.com>
2013-12-06 11:31:16 +0100

    Simplified protractor e2e tests

--- test/e2e/01-centerExample.js
+++ test/e2e/01-centerExample.js
@@ -5,21 +5,21 @@ describe('Loading center-example.html', function() {
     var ptor, driver;
     beforeEach(function() {
         ptor = protractor.getInstance();
-        ptor.get('/center-example.html');
+        browser.get('/center-example.html');
         driver = ptor.driver;
     }, 30000);
 
     it('should update the zoom value in the input if clicked the zoom control', function() {
-        driver.findElement(protractor.By.className('leaflet-control-zoom-in')).click();
+        element(by.className('leaflet-control-zoom-in')).click();
         // Wait for zoom animation
         ptor.sleep(300);
-        driver.findElement(protractor.By.xpath('/html/body/form/input[3]')).getAttribute("value").then(function(zoom) {
+        element(by.xpath('/html/body/form/input[3]')).getAttribute("value").then(function(zoom) {
             expect(zoom).toBe('5');
         });
     });
 
     xit('should update the zoom value if the map is dragged', function() {
-        driver.findElement(protractor.By.xpath('/html/body/form/input[3]')).sendKeys("2");
+        element(by.xpath('/html/body/form/input[3]')).sendKeys("2");
         ptor.sleep(3000);
     });
 
