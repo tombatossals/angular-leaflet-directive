f9fecf3b749e7ce025f57766496efceb75f8f8de
David Rubert <david.rubert@gmail.com>
2013-12-13 10:06:05 +0100

    Updated grunt-bump configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
                 tagName: 'v%VERSION%',
                 tagMessage: 'Version %VERSION%',
                 push: true,
-                pushTo: 'upstream',
+                pushTo: 'origin',
                 gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
             }
         },
