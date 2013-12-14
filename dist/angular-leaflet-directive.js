d95193c2fe5a03050c23e1e0c33db38ae5f44eea
tombatossals <david.rubert@gmail.com>
2013-12-14 17:29:32 +0100

    New compiled version

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -405,23 +405,30 @@ angular.module("leaflet-directive").directive('layers', function ($log, $q, leaf
                 leafletLayers.controls = {};
                 leafletLayers.controls.layers = new L.control.layers();
                 leafletLayers.controls.layers.setPosition(defaults.controlLayersPosition);
-                leafletLayers.controls.layers.addTo(map);
 
 
                 // Setup all baselayers definitions
                 var top = false;
                 for (var layerName in layers.baselayers) {
                     var newBaseLayer = createLayer(layers.baselayers[layerName]);
-                    if (isDefined(newBaseLayer)) {
-                        leafletLayers.baselayers[layerName] = newBaseLayer;
-                        // Only add the visible layer to the map, layer control manages the addition to the map
-                        // of layers in its control
-                        if (layers.baselayers[layerName].top === true) {
-                            map.addLayer(leafletLayers.baselayers[layerName]);
-                            top = true;
-                        }
-                        leafletLayers.controls.layers.addBaseLayer(leafletLayers.baselayers[layerName], layers.baselayers[layerName].name);
+                    if (!isDefined(newBaseLayer)) {
+                        delete layers.baselayers[layerName];
+                        continue;
                     }
+                    leafletLayers.baselayers[layerName] = newBaseLayer;
+                    // Only add the visible layer to the map, layer control manages the addition to the map
+                    // of layers in its control
+                    if (layers.baselayers[layerName].top === true) {
+                        map.addLayer(leafletLayers.baselayers[layerName]);
+                        top = true;
+                    }
+
+                    leafletLayers.controls.layers.addBaseLayer(leafletLayers.baselayers[layerName], layers.baselayers[layerName].name);
+                }
+
+                // Only add the layers switch selector control if we have more than one baselayer
+                if (Object.keys(layers.baselayers).length > 1) {
+                    leafletLayers.controls.layers.addTo(map);
                 }
 
                 // If there is no visible layer add first to the map
