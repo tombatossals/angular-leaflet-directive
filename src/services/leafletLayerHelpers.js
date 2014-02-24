9d769a2c3aa14a43fba68b774202fb487ceb5970
JohnPhoto <john@johnphoto.se>
2014-02-24 09:51:06 +0100

    Added China-provider.

--- src/services/leafletLayerHelpers.js
+++ src/services/leafletLayerHelpers.js
@@ -63,6 +63,16 @@ angular.module("leaflet-directive").factory('leafletLayerHelpers', function ($ro
                 return new L.Google(type, params.options);
             }
         },
+        china:{
+            mustHaveUrl:false,
+            createLayer:function(params){
+                var type = params.type || '';
+                if(!Helpers.ChinaLayerPlugin.isLoaded()){
+                    return;
+                }
+                return L.tileLayer.chinaProvider(type, params.options);
+            }
+        },
         ags: {
             mustHaveUrl: true,
             createLayer: function(params) {
