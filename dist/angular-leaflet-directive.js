f9bb00bc9276ac74e31a66a6556c578ace35365a
tombatossals <david.rubert@gmail.com>
2014-03-16 16:10:18 +0100

    New library version

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -823,6 +823,10 @@
             leafletScope.$watch('paths', function (newPaths) {
               // Create the new paths
               for (var newName in newPaths) {
+                if (newName.search('-') !== -1) {
+                  $log.error('[AngularJS - Leaflet] The path name "' + newName + '" is not valid. It must not include "-" and a number.');
+                  continue;
+                }
                 if (!isDefined(leafletPaths[newName])) {
                   var pathData = newPaths[newName];
                   var newPath = createPath(newName, newPaths[newName], defaults);
