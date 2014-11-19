bb73c7c1fa7e7e6b7ae7e68110dc0304a0e547eb
fbarnard <fbarnard@gmail.com>
2014-11-19 17:23:39 +0200

    Updated Legend.js
    Updating of legendClass/position variable when legend changes
    Removing of leafletLegend when legend gets set to undefined

--- src/directives/legend.js
+++ src/directives/legend.js
@@ -5,54 +5,97 @@ angular.module("leaflet-directive").directive('legend', function ($log, $http, l
         replace: false,
         require: 'leaflet',
 
-        link: function(scope, element, attrs, controller) {
-            var isArray      = leafletHelpers.isArray,
+        link: function (scope, element, attrs, controller) {
+
+            var isArray = leafletHelpers.isArray,
                 isDefined = leafletHelpers.isDefined,
                 isFunction = leafletHelpers.isFunction,
                 leafletScope = controller.getLeafletScope(),
-                legend       = leafletScope.legend;
+                legend = leafletScope.legend;
 
-            var legendClass = legend.legendClass ? legend.legendClass : "legend";
-            var position = legend.position || 'bottomright';
+            var legendClass;
+            var position;
             var leafletLegend;
 
-            controller.getMap().then(function(map) {
-                leafletScope.$watch('legend', function (legend) {
-                    if (!isDefined(legend.url) && (!isArray(legend.colors) || !isArray(legend.labels) || legend.colors.length !== legend.labels.length)) {
-                        $log.warn("[AngularJS - Leaflet] legend.colors and legend.labels must be set.");
-                    } else if(isDefined(legend.url)){
-                        $log.info("[AngularJS - Leaflet] loading arcgis legend service.");
-                    } else {
-                        if (isDefined(leafletLegend)) {
-						    leafletLegend.removeFrom(map);
-						}
-                        leafletLegend = L.control({ position: position });
-                        leafletLegend.onAdd = leafletLegendHelpers.getOnAddArrayLegend(legend, legendClass);
-                        leafletLegend.addTo(map);
-                    }
-                });
+            controller.getMap().then(function (map) {
 
-                leafletScope.$watch('legend.url', function(newURL) {
-                    if(!isDefined(newURL)) {
+                leafletScope.$watch('legend', function (newLegend) {
+
+                    if (isDefined(newLegend)) {
+
+                        legendClass = newLegend.legendClass ? newLegend.legendClass : "legend";
+
+                        position = newLegend.position || 'bottomright';
+                    }
+
+                }, true);
+
+
+                leafletScope.$watch('legend', function (newLegend) {
+
+                    if (!isDefined(newLegend)) {
+
+                        if (isDefined(leafletLegend)) {
+                            leafletLegend.removeFrom(map);
+                        }
+
                         return;
                     }
+
+
+                    if (!isDefined(newLegend.url) && (!isArray(newLegend.colors) || !isArray(newLegend.labels) || newLegend.colors.length !== newLegend.labels.length)) {
+
+                        $log.warn("[AngularJS - Leaflet] legend.colors and legend.labels must be set.");
+
+                        return;
+                    }
+
+                    if (isDefined(newLegend.url)) {
+
+                        $log.info("[AngularJS - Leaflet] loading arcgis legend service.");
+
+                        return;
+                    }
+
+                    if (isDefined(leafletLegend)) {
+                        leafletLegend.removeFrom(map);
+                    }
+
+                    leafletLegend = L.control({position: position});
+                    leafletLegend.onAdd = leafletLegendHelpers.getOnAddArrayLegend(newLegend, legendClass);
+                    leafletLegend.addTo(map);
+
+                });
+
+                leafletScope.$watch('legend.url', function (newURL) {
+
+                    if (!isDefined(newURL)) {
+                        return;
+                    }
+
                     $http.get(newURL)
-                        .success(function(legendData) {
-                            if(isDefined(leafletLegend)) {
-                                leafletLegendHelpers.updateArcGISLegend(leafletLegend.getContainer(),legendData);
+                        .success(function (legendData) {
+
+                            if (isDefined(leafletLegend)) {
+
+                                leafletLegendHelpers.updateArcGISLegend(leafletLegend.getContainer(), legendData);
+
                             } else {
-                                leafletLegend = L.control({ position: position });
+
+                                leafletLegend = L.control({position: position});
                                 leafletLegend.onAdd = leafletLegendHelpers.getOnAddArcGISLegend(legendData, legendClass);
                                 leafletLegend.addTo(map);
                             }
-                            if(isDefined(legend.loadedData) && isFunction(legend.loadedData)) {
+
+                            if (isDefined(legend.loadedData) && isFunction(legend.loadedData)) {
                                 legend.loadedData();
                             }
                         })
-                        .error(function() {
+                        .error(function () {
                             $log.warn('[AngularJS - Leaflet] legend.url not loaded.');
                         });
                 });
+
             });
         }
     };
