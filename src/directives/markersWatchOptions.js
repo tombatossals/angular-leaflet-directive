8fdd948fd90d8b53bc9c5b65ff003ce29726d8d4
Nick McCready <nmccready@gmail.com>
2015-04-06 11:19:28 -0400

    delay markersWatchOptions until map is ready

--- src/directives/markersWatchOptions.js
+++ src/directives/markersWatchOptions.js
@@ -1,30 +1,31 @@
 angular.module("leaflet-directive").directive('markersWatchOptions',
     function ($log, $rootScope, $q, leafletData, leafletHelpers, leafletMarkersHelpers) {
-    //less terse vars to helpers
-    var isDefined = leafletHelpers.isDefined,
-        errorHeader = leafletHelpers.errorHeader,
-        isObject = leafletHelpers.isObject,
-        _markersWatchOptions = leafletMarkersHelpers.markersWatchOptions;
+        //less terse vars to helpers
+        var isDefined = leafletHelpers.isDefined,
+            errorHeader = leafletHelpers.errorHeader,
+            isObject = leafletHelpers.isObject,
+            _markersWatchOptions = leafletMarkersHelpers.markersWatchOptions;
 
-    return {
-        restrict: "A",
-        scope: false,
-        replace: false,
-        require: ['leaflet'],
+        return {
+            restrict: "A",
+            scope: false,
+            replace: false,
+            require: ['leaflet'],
 
-        link: function(scope, element, attrs, controller) {
-            var mapController = controller[0],
-                leafletScope = mapController.getLeafletScope();
+            link: function (scope, element, attrs, controller) {
+                var mapController = controller[0],
+                    leafletScope = mapController.getLeafletScope();
 
-
-                if(isDefined(scope.markersWatchOptions)) {
-                    if(isObject(scope.markersWatchOptions))
-                        angular.extend(_markersWatchOptions, scope.markersWatchOptions);
-                    else
-                        $log.error(errorHeader + ' [markers] markersWatchOptions is not an object');
-                    leafletScope.markersWatchOptions = _markersWatchOptions;
-                }
-        }
-    };
+                mapController.getMap().then(function () {
+                    if (isDefined(scope.markersWatchOptions)) {
+                        if (isObject(scope.markersWatchOptions))
+                            angular.extend(_markersWatchOptions, scope.markersWatchOptions);
+                        else
+                            $log.error(errorHeader + ' [markers] markersWatchOptions is not an object');
+                        leafletScope.markersWatchOptions = _markersWatchOptions;
+                    }
+                });
+            }
+        };
 });
 
