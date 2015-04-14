85b79b7c1a57d37a44a4fb5c4fdc7c0acc98ca21
Nick McCready <nmccready@gmail.com>
2015-04-14 12:34:05 -0400

    Release v0.7.13

--- dist/angular-leaflet-directive_dev_mapped.js
+++ dist/angular-leaflet-directive_dev_mapped.js
@@ -1,5 +1,5 @@
 /*!
-*  angular-leaflet-directive 0.7.12 2015-04-14
+*  angular-leaflet-directive 0.7.13 2015-04-14
 *  angular-leaflet-directive - An AngularJS directive to easily interact with Leaflet maps
 *  git: https://github.com/tombatossals/angular-leaflet-directive
 */
