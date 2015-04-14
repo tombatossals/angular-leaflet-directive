f6f062fa5ea09d36fa46102d3198780390616591
Nick McCready <nmccready@gmail.com>
2015-04-14 11:59:00 -0400

    grunt bump bower syncing

--- grunt/bump.js
+++ grunt/bump.js
@@ -3,11 +3,11 @@
 module.exports = function (grunt, options) {
     return {
         options: {
-            files: ['package.json'],
+            files: ['package.json', 'bower.json'],
             updateConfigs: [],
             commit: true,
             commitMessage: 'Release v%VERSION%',
-            commitFiles: ['package.json'],
+            commitFiles: ['package.json', 'bower.json', 'dist/*'],
             createTag: true,
             tagName: 'v%VERSION%',
             tagMessage: 'Version %VERSION%',
