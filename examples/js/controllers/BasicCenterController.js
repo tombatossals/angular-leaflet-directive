c8baca5d0e98eb14b0aad4e59e5bf97c620728b3
tombatossals <david.rubert@gmail.com>
2015-04-18 10:19:37 +0200

    deleted

--- examples/js/controllers/BasicCenterController.js
+++
@@ -1,9 +0,0 @@
-        app.controller('BasicCenterController', [ '$scope', function($scope) {
-            angular.extend($scope, {
-                london: {
-                    lat: 51.505,
-                    lng: -0.09,
-                    zoom: 4
-                }
-            });
-       }]);
--- examples/js/controllers/BasicDynamicAddRemoveMapExample.js
+++
@@ -1,8 +0,0 @@
-        app.config(function ($routeProvider) {
-            $routeProvider.when('/map', {
-                template: '<leaflet width="100%" height="480px"></leaflet>',
-                controller: 'BasicDynamicAddRemoveMapExample'
-            });
-        });
-        app.controller('BasicDynamicAddRemoveMapExample', [ '$scope', 'leafletData', function($scope, leafletData) {
-        } ]);
