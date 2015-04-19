d847b05aae1c9668c0edd514d507e615c9f6dbd0
tombatossals <david.rubert@gmail.com>
2015-04-19 16:04:26 +0200

    Added the new example to the example viewer

--- examples/js/controllers.js
+++ examples/js/controllers.js
@@ -1273,6 +1273,49 @@ var app = angular.module('webapp');
                 }
             });
         }]);
+        app.controller("LayersHeatmapController", ["$scope", "$http", function($scope, $http) {
+            var points = [];
+            var heatmap = {
+                name: 'Heat Map',
+                type: 'heat',
+                data: points,
+                visible: true
+            };
+            $http.get("json/heat-points.json").success(function(data) {
+                $scope.layers.overlays = {
+                    heat: {
+                        name: 'Heat Map',
+                        type: 'heat',
+                        data: data,
+                        layerOptions: {
+                            radius: 20,
+                            blur: 10
+                        },
+                        visible: true
+                    }
+                };
+            });
+            angular.extend($scope, {
+                center: {
+                    lat: 37.774546,
+                    lng: -122.433523,
+                    zoom: 12
+                },
+                layers: {
+                    baselayers: {
+                        mapbox_light: {
+                            name: 'Mapbox Light',
+                            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                            type: 'xyz',
+                            layerOptions: {
+                                apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+                                mapid: 'bufanuvols.lia22g09'
+                            }
+                        }
+                    }
+                }
+            });
+        }]);
         app.controller("LayersHideBaselayerOnSelectorController", [ "$scope", function($scope) {
             angular.extend($scope, {
                 center: {
@@ -1971,7 +2014,7 @@ var app = angular.module('webapp');
                     overlays: {
                         heatmap: {
                             name: 'Heat Map',
-                            type: 'heatmap',
+                            type: 'webGLHeatmap',
                             data: dataPoints,
                             visible: true
                         }
