0b885666d57f537cd5166c1faf260d0aa64a59fe
Nick McCready <nmccready@gmail.com>
2015-06-14 17:06:23 -0400

    fix(popups position markers): POST BUILD/MERGE popups position after being compiled. Refactored open popup functionality

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -1,5 +1,5 @@
 /*!
-*  angular-leaflet-directive 0.8.3 2015-06-11
+*  angular-leaflet-directive 0.8.3 2015-06-14
 *  angular-leaflet-directive - An AngularJS directive to easily interact with Leaflet maps
 *  git: https://github.com/tombatossals/angular-leaflet-directive
 */
@@ -1748,10 +1748,7 @@ angular.module("leaflet-directive").factory('leafletMapDefaults', ["$q", "leafle
     };
 }]);
 
-angular.module("leaflet-directive")
-.service('leafletMarkersHelpers', ["$rootScope", "$timeout", "leafletHelpers", "$log", "$compile", "leafletGeoJsonHelpers", function ($rootScope, $timeout, leafletHelpers, $log, $compile,
-                                            leafletGeoJsonHelpers) {
-
+angular.module("leaflet-directive").service('leafletMarkersHelpers', ["$rootScope", "$timeout", "leafletHelpers", "$log", "$compile", "leafletGeoJsonHelpers", function ($rootScope, $timeout, leafletHelpers, $log, $compile, leafletGeoJsonHelpers) {
     var isDefined = leafletHelpers.isDefined,
         defaultTo = leafletHelpers.defaultTo,
         MarkerClusterPlugin = leafletHelpers.MarkerClusterPlugin,
@@ -1869,47 +1866,59 @@ angular.module("leaflet-directive")
         }
     };
 
-    var _manageOpenPopup = function (marker, markerData) {
-        marker.openPopup();
+    var adjustPopupPan = function(marker, map) {
+        var containerHeight = marker._popup._container.offsetHeight,
+            layerPos = new L.Point(marker._popup._containerLeft, -containerHeight - marker._popup._containerBottom),
+            containerPos = map.layerPointToContainerPoint(layerPos);
+        if (containerPos !== null) {
+            marker._popup._adjustPan();
+        }
+    };
 
+    var compilePopup = function(marker, markerScope) {
+        $compile(marker._popup._contentNode)(markerScope);
+    };
+
+    var updatePopup = function (marker, markerScope, map) {
+        //The innerText should be more than 1 once angular has compiled.
+        //We need to keep trying until angular has compiled before we _updateLayout and _updatePosition
+        //This should take care of any scenario , eg ngincludes, whatever.
+        //Is there a better way to check for this?
+        if (marker._popup._contentNode.innerText.length < 1) {
+            $timeout(function () {
+                updatePopup(marker, markerScope, map);
+            });
+        }
+
+        //cause a reflow - this is also very important - if we don't do this then the widths are from before $compile
+        var reflow = marker._popup._contentNode.offsetWidth;
+
+        marker._popup._updateLayout();
+        marker._popup._updatePosition();
+
+        if (marker._popup.options.autoPan) {
+            adjustPopupPan(marker, map);
+        }
+
+        //using / returning reflow so jshint doesn't moan
+        return reflow;
+    };
+
+    var _manageOpenPopup = function (marker, markerData, map) {
         // The marker may provide a scope returning function used to compile the message
         // default to $rootScope otherwise
         var markerScope = angular.isFunction(markerData.getMessageScope) ? markerData.getMessageScope() : $rootScope,
             compileMessage = isDefined(markerData.compileMessage) ? markerData.compileMessage : true;
 
-        var compileAndUpdatePopup = function (popup) {
-            if (!isDefined(popup) || !isDefined(popup._contentNode)) {
-                return;
+        if (compileMessage) {
+            if (!isDefined(marker._popup) || !isDefined(marker._popup._contentNode)) {
+                $log.error(errorHeader + 'Popup is invalid or does not have any content.');
+                return false;
             }
 
-            var updatePopup = function (popup) {
-                popup._updateLayout();
-                popup._updatePosition();
-                if (popup.options.autoPan) {
-                    popup._adjustPan();
-                }
-            };
-
-            $compile(popup._contentNode)(markerScope);
-
-            // In case of an ng-include, we need to update the content after template load
-            if (popup._contentNode.innerHTML.indexOf("ngInclude") > -1) {
-                var unregister = markerScope.$on('$includeContentLoaded', function () {
-                    $timeout(function () {
-                        updatePopup(popup);
-                        unregister();
-                    });
-                });
-            }
-            else {
-                // We need to wait until after the next draw in order to get the correct width
-                $timeout(function () {
-                    updatePopup(popup);
-                });
-            }
-        };
-        if (compileMessage)
-            compileAndUpdatePopup(marker.getPopup());
+            compilePopup(marker, markerScope);
+            updatePopup(marker, markerData, map);
+        }
     };
 
     var _manageOpenLabel = function (marker, markerData) {
@@ -1992,13 +2001,15 @@ angular.module("leaflet-directive")
             groups[groupName].addLayer(marker);
         },
 
-        listenMarkerEvents: function (marker, markerData, leafletScope) {
+        listenMarkerEvents: function (marker, markerData, leafletScope, doWatch, map) {
             //these should be deregistered on destroy .. possible leake
             //handles should not be closures since they will need to be removed
             marker.on("popupopen", function (/* event */) {
                 safeApply(leafletScope, function () {
-                    markerData.focus = true;
-                    _manageOpenPopup(marker, markerData);//needed since markerData is now a copy
+                    if (isDefined(marker._popup) || isDefined(marker._popup._contentNode)) {
+                        markerData.focus = true;
+                        _manageOpenPopup(marker, markerData, map);//needed since markerData is now a copy
+                    }
                 });
             });
             marker.on("popupclose", function (/* event */) {
@@ -2094,7 +2105,7 @@ angular.module("leaflet-directive")
                     // The marker is automatically added to the map depending on the visibility
                     // of the layer, so we only have to open the popup if the marker is in the map
                     if (map.hasLayer(marker) && markerData.focus === true) {
-                        _manageOpenPopup(marker, markerData);
+                        _manageOpenPopup(marker, markerData, map);
                     }
                 }
 
@@ -2192,7 +2203,7 @@ angular.module("leaflet-directive")
                 // The markerData.focus property must be true so we update if there wasn't a previous value or it wasn't true
                 if (markerData.focus === true && ( !isDefined(oldMarkerData.focus) || oldMarkerData.focus === false) || (isInitializing && markerData.focus === true)) {
                     // Reopen the popup when focus is still true
-                    _manageOpenPopup(marker, markerData);
+                    _manageOpenPopup(marker, markerData, map);
                     updatedFocus = true;
                 }
 
@@ -3639,8 +3650,8 @@ angular.module("leaflet-directive").directive('legend', ["$log", "$http", "leafl
 
 angular.module("leaflet-directive").directive('markers',
     ["$log", "$rootScope", "$q", "leafletData", "leafletHelpers", "leafletMapDefaults", "leafletMarkersHelpers", "leafletMarkerEvents", "leafletIterators", "leafletWatchHelpers", "leafletDirectiveControlsHelpers", function ($log, $rootScope, $q, leafletData, leafletHelpers, leafletMapDefaults,
-      leafletMarkersHelpers, leafletMarkerEvents, leafletIterators, leafletWatchHelpers,
-      leafletDirectiveControlsHelpers) {
+              leafletMarkersHelpers, leafletMarkerEvents, leafletIterators, leafletWatchHelpers,
+              leafletDirectiveControlsHelpers) {
     //less terse vars to helpers
     var isDefined = leafletHelpers.isDefined,
         errorHeader = leafletHelpers.errorHeader,
@@ -3684,7 +3695,7 @@ angular.module("leaflet-directive").directive('markers',
         // The marker is automatically added to the map depending on the visibility
         // of the layer, so we only have to open the popup if the marker is in the map
         if (!doIndividualWatch && map.hasLayer(marker) && model.focus === true) {
-            leafletMarkersHelpers.manageOpenPopup(marker, model);
+            leafletMarkersHelpers.manageOpenPopup(marker, model, map);
         }
         return true;
     };
@@ -3741,7 +3752,7 @@ angular.module("leaflet-directive").directive('markers',
                     // We do not have a layer attr, so the marker goes to the map layer
                     map.addLayer(marker);
                     if (!watchOptions.individual.doWatch && model.focus === true) {
-                        leafletMarkersHelpers.manageOpenPopup(marker, model);
+                        leafletMarkersHelpers.manageOpenPopup(marker, model, map);
                     }
                 }
                 var pathToMarker = Helpers.getObjectDotPath(maybeLayerName? [maybeLayerName, newName]: [newName]);
@@ -3750,16 +3761,16 @@ angular.module("leaflet-directive").directive('markers',
                         watchOptions.individual.doWatch);
                 }
 
-                listenMarkerEvents(marker, model, leafletScope, watchOptions.individual.doWatch);
+                listenMarkerEvents(marker, model, leafletScope, watchOptions.individual.doWatch, map);
                 leafletMarkerEvents.bindEvents(marker, pathToMarker, model, leafletScope, layerName);
             }
         }
     };
     var _seeWhatWeAlreadyHave = function(markerModels, oldMarkerModels, lMarkers, isEqual, cb){
         var hasLogged = false,
-          equals = false,
-          oldMarker,
-          newMarker;
+            equals = false,
+            oldMarker,
+            newMarker;
 
         var doCheckOldModel =  isDefined(oldMarkerModels);
         for (var name in lMarkers) {
@@ -3769,19 +3780,19 @@ angular.module("leaflet-directive").directive('markers',
             }
 
             if(doCheckOldModel){
-              //might want to make the option (in watch options) to disable deep checking
-              //ie the options to only check !== (reference check) instead of angular.equals (slow)
-              newMarker = markerModels[name];
-              oldMarker = oldMarkerModels[name];
-              equals = angular.equals(newMarker,oldMarker) && isEqual;
+                //might want to make the option (in watch options) to disable deep checking
+                //ie the options to only check !== (reference check) instead of angular.equals (slow)
+                newMarker = markerModels[name];
+                oldMarker = oldMarkerModels[name];
+                equals = angular.equals(newMarker,oldMarker) && isEqual;
             }
             if (!isDefined(markerModels) ||
                 !Object.keys(markerModels).length ||
                 !isDefined(markerModels[name]) ||
                 !Object.keys(markerModels[name]).length ||
                 equals) {
-                    if(cb && Helpers.isFunction(cb))
-                        cb(newMarker, oldMarker, name);
+                if(cb && Helpers.isFunction(cb))
+                    cb(newMarker, oldMarker, name);
             }
         }
     };
@@ -3791,7 +3802,7 @@ angular.module("leaflet-directive").directive('markers',
                 $log.debug(errorHeader + '[marker] is deleting marker: ' + lMarkerName);
                 deleteMarker(lMarkers[lMarkerName], map, layers);
                 delete lMarkers[lMarkerName];
-        });
+            });
     };
 
     var _getNewModelsToSkipp =  function(newModels, oldModels, lMarkers){
@@ -3800,7 +3811,7 @@ angular.module("leaflet-directive").directive('markers',
             function(newMarker, oldMarker, lMarkerName){
                 $log.debug(errorHeader + '[marker] is already rendered, marker: ' + lMarkerName);
                 skips[lMarkerName] = newMarker;
-        });
+            });
         return skips;
     };
 
@@ -3832,7 +3843,7 @@ angular.module("leaflet-directive").directive('markers',
 
                 // backwards compat
                 if(isDefined(attrs.watchMarkers))
-                  watchOptions.doWatch = watchOptions.individual.doWatch =
+                    watchOptions.doWatch = watchOptions.individual.doWatch =
                         (!isDefined(attrs.watchMarkers) || Helpers.isTruthy(attrs.watchMarkers));
 
                 var isNested = (isDefined(attrs.markersNested) && Helpers.isTruthy(attrs.markersNested));
