b1b85debca0ff56713268e1795bdc80b8834bf90
David Rubert <david.rubert@gmail.com>
2015-07-01 14:57:43 +0200

    reworked controls management

--- src/directives/controls.js
+++ src/directives/controls.js
@@ -1,4 +1,4 @@
-angular.module("leaflet-directive").directive('controls', function ($log, leafletHelpers, leafletLayerHelpers) {
+angular.module("leaflet-directive").directive('controls', function ($log, leafletHelpers, leafletControlHelpers) {
     return {
         restrict: "A",
         scope: false,
@@ -10,80 +10,19 @@ angular.module("leaflet-directive").directive('controls', function ($log, leafle
                 return;
             }
 
-            var isDefined = leafletHelpers.isDefined,
-                createLayer = leafletLayerHelpers.createLayer,
+            var createControl = leafletControlHelpers.createControl,
                 leafletScope  = controller.getLeafletScope(),
-                controls = leafletScope.controls,
-                errorHeader = leafletHelpers.errorHeader + ' [Controls] ';
+                controls = leafletScope.controls;
 
             controller.getMap().then(function(map) {
-                if (isDefined(L.Control.Draw) && isDefined(controls.draw)) {
-
-                    if (!isDefined(controls.edit)) {
-                        controls.edit = { featureGroup: new L.FeatureGroup() };
-                        map.addLayer(controls.edit.featureGroup);
-                    }
-
-                    var drawControl = new L.Control.Draw(controls);
-                    map.addControl(drawControl);
-                }
-
-                if (isDefined(controls.scale)) {
-                    var scaleControl = new L.control.scale(controls.scale);
-                    map.addControl(scaleControl);
-                }
-
-                if (isDefined(controls.fullscreen)) {
-                    if (leafletHelpers.FullScreenControlPlugin.isLoaded()) {
-                        var fullscreenControl = new L.Control.Fullscreen(controls.fullscreen);
-                        map.addControl(fullscreenControl);
+                for (var controlType in controls) {
+                    var control;
+                    if (controlType !== 'custom') {
+                        control = createControl(controlType, controls[controlType]);
                     } else {
-                        $log.error(errorHeader + ' Fullscreen plugin is not loaded.');
+                        control = controls[controlType];
                     }
-                }
-
-                if (isDefined(controls.search)) {
-                    if (leafletHelpers.SearchControlPlugin.isLoaded()) {
-                        var searchControl = new L.Control.Search(controls.search);
-                        map.addControl(searchControl);
-                    } else {
-                        $log.error(errorHeader + ' Search plugin is not loaded.');
-                    }
-                }
-
-                if(isDefined(controls.minimap)) {
-                    if (leafletHelpers.MiniMapControlPlugin.isLoaded()) {
-                        if(isDefined(controls.minimap.layer)) {
-                            var layer = createLayer(controls.minimap.layer);
-                            delete controls.minimap.layer;
-
-                            if(isDefined(layer)) {
-                                if(isDefined(leafletScope.center)) {
-                                    var moveend = function(/* event */) {
-                                        var minimapControl = new L.Control.MiniMap(layer, controls.minimap);
-                                        map.addControl(minimapControl);
-                                        map.off('moveend', moveend);
-                                    };
-                                    map.on('moveend', moveend);
-                                } else {
-                                    var minimapControl = new L.Control.MiniMap(layer, controls.minimap);
-                                    map.addControl(minimapControl);
-                                }
-                            } else {
-                                $log.warn(errorHeader + ' Layer could not be created.');
-                            }
-                        } else {
-                            $log.warn(errorHeader +' Layer option should be defined.');
-                        }
-                    } else {
-                        $log.error(errorHeader + ' Minimap plugin is not loaded.');
-                    }
-                }
-
-                if (isDefined(controls.custom)) {
-                    for(var i in controls.custom) {
-                        map.addControl(controls.custom[i]);
-                    }
+                    map.addControl(control);
                 }
             });
         }
