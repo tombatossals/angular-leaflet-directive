674594c6992c05a322e12086bc61c6e4272bd21e
David Rubert <david.rubert@gmail.com>
2015-11-06 14:37:31 +0100

    fix(tests): Clean and better structured tests, no coffeescript.

--- test/e2e/0100-basic-first-example.js
+++ test/e2e/0100-basic-first-example.js
@@ -2,17 +2,17 @@
 
 describe('Loading 0100-basic-first-example.html', function() {
 
-    beforeEach(function() {
-        browser.get('0100-basic-first-example.html');
-        browser.wait(function() {
-            return element(by.css('img.leaflet-tile-loaded')).isPresent();
-        }, 5000);
-    }, 30000);
+  beforeEach(function() {
+    browser.get('0100-basic-first-example.html');
+    browser.wait(function() {
+      return element(by.css('img.leaflet-tile-loaded')).isPresent();
+    }, 5000);
+  }, 30000);
 
-    it('should load the Leaflet map inside the directive tag', function() {
-        element(by.className('angular-leaflet-map')).getText().then(function(text) {
-            expect(text).toBe("+\n-\nLeaflet | © OpenStreetMap contributors");
-        });
+  it('should load the Leaflet map inside the directive tag', function() {
+    element(by.className('angular-leaflet-map')).getText().then(function(text) {
+      expect(text).toBe('+\n-\nLeaflet | © OpenStreetMap contributors');
     });
+  });
 
 });
