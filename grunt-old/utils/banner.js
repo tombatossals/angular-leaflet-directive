85285824f986d99710e16a5549988666c13acaac
David Rubert <david.rubert@gmail.com>
2015-11-06 13:13:05 +0100

    fix(grunt): Removed old and unneeded grunt tasks

--- grunt-old/utils/banner.js
+++
@@ -1,6 +0,0 @@
-module.exports =
-    '/*!\n' +
-    '*  <%= pkg.name %> <%= pkgFunction().version %> <%= grunt.template.today(\"yyyy-mm-dd\") %>\n' +
-    '*  <%= pkg.description %>\n' +
-    '*  <%= pkg.repository.type %>: <%= pkg.repository.url %>\n' +
-    '*/\n'
